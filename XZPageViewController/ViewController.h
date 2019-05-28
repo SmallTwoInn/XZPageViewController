45a9df4ea69c07fe51acf3ede3960160bb7cc238
zhuxuezhen@offcn.com <zhuxuezhen@offcn.com>
2019-05-28 17:25:05 +0800

    初始化项目

---
+++ XZPageViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XZPageViewController
+//
+//  Created by Yeso on 2019/5/28.
+//  Copyright © 2019年 Yeso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XZPageViewController/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  XZPageViewController
+//
+//  Created by Yeso on 2019/5/28.
+//  Copyright © 2019年 Yeso. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
